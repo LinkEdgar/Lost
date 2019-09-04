1aa8f995ee227a5708c38d3238c164c9e8c5d7b0
Reyes <ereyes@m-c02yg123jg5j.lan>
2019-09-03 23:09:38 -0400

    Added glide dependecy. Created LostCard. Added pet images

--- app/src/main/java/com/example/lost/Fragments/IntroFragment.kt
+++ app/src/main/java/com/example/lost/Fragments/IntroFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.lost.Adapters.IntroAdapter
 import com.example.lost.R
+import kotlinx.android.synthetic.main.adopt_card.view.*
 import kotlinx.android.synthetic.main.fragment_intro.view.*
 
 class IntroFragment : Fragment(){
@@ -27,6 +29,12 @@ class IntroFragment : Fragment(){
         adapter = IntroAdapter(context!!, list)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context!!)
+
+        Glide.with(rootView).load(R.drawable.shiba_inu).centerCrop().into(rootView.adopt_card.petImage)
+
+        Glide.with(rootView).load(R.drawable.wolf).centerCrop().into(rootView.lost_card.petImage)
+
+
         return rootView
     }
 }
