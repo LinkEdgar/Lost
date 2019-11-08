f69193c9eccdee304d8b6a509a33016fee94d94b
Reyes <ereyes@m-c02yg123jg5j.lan>
2019-11-08 18:01:00 -0500

    Added a search fragment. Added bottom navigation

--- app/src/main/java/com/example/lost/Activities/MainActivity.kt
+++ app/src/main/java/com/example/lost/Activities/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.lost.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.lost.R
+import com.example.lost.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(){
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/lost/Fragments/PetSearchFragment.kt
@@ -0,0 +1,17 @@
+package com.example.lost.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.lost.R
+
+class PetSearchFragment: Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_pet_search,container,false)
+
+        return rootView
+    }
+}
