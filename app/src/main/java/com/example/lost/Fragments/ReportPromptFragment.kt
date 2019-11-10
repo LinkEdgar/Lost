f0744a29d1b75e0542b193d5b2976613f345b56d
Reyes <ereyes@m-c02yg123jg5j.lan>
2019-11-09 23:07:52 -0500

    Added Report form so users can add pets to the DB

--- app/src/main/java/com/example/lost/Fragments/ReportPromptFragment.kt
+++ app/src/main/java/com/example/lost/Fragments/ReportPromptFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.lost.R
+import kotlinx.android.synthetic.main.fragment_report_prompt.view.*
 
 class ReportPromptFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val rootView = inflater.inflate(R.layout.fragment_report_prompt, container, false)
+        rootView.next_bt.setOnClickListener{
+            view!!.findNavController().navigate(R.id.action_reportPromptFragment_to_petFormFragment)
+        }
         return rootView
     }
 }
